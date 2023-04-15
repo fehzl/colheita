7c77f7690915251cfc19d1d43416196c57003fa7
Felipe Feliciano <fehzl@live.com>
2023-04-15 01:56:37 -0300

    refactor: remove return to initial position

--- colheita.lua
+++ colheita.lua
@@ -4,7 +4,7 @@ local sides = require("sides")
 
 -- Constants
 local rows = 9
-local columns = 21
+local columns = 23
 local waitTime = 600 -- 10 minutes in seconds
 
 -- Function to move forward
@@ -22,6 +22,10 @@ end
 -- Function to turn the robot to the left
 local function turnLeft()
   robot.turnLeft()
+end
+
+local function turnBack()
+  robot.turnAround()
 end
 
 -- Function to harvest the block in front of the robot
@@ -65,40 +69,12 @@ local function harvestCycle()
   end
 end
 
--- Function to perform the entire harvest cycle in reverse
-local function harvestCycleReverse()
-  local isEvenRow = (rows % 2 == 0)
-
-  for i = 1, rows do
-    harvestRow()
-
-    if i < rows then
-      moveToNextRow(isEvenRow)
-      isEvenRow = not isEvenRow
-    end
-  end
-end
-
--- Function to return the robot to its initial position
-local function returnToInitialPosition()
-  -- Turn the robot to face the first row
-  turnLeft()
-
-  -- Go back to the first row
-  for i = 1, rows - 1 do
-    moveForward()
-  end
-
-  -- Turn the robot to face the initial direction
-  turnLeft()
-end
-
 -- Main function
 local function main()
   while true do
     harvestCycle()
-    harvestCycleReverse()
-    returnToInitialPosition()
+    turnBack()
+    harvestCycle()
     os.sleep(waitTime)
   end
 end
